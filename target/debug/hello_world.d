21002b8a2f5310b24d085bc0ca05829787cbe472
TimDrogin <tim.drogin@gmail.com>
2023-07-14 23:53:50 +0100

    Remove files ignored by .gitignore

--- target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Tim\ProgProjects\chess-engine\rusty_chess_engine\target\debug\hello_world.exe: C:\Users\Tim\ProgProjects\chess-engine\rusty_chess_engine\src\main.rs
--- target/debug/rusty_chess_engine.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Tim\ProgProjects\chess-engine\rusty_chess_engine\target\debug\rusty_chess_engine.exe: C:\Users\Tim\ProgProjects\chess-engine\rusty_chess_engine\src\main.rs
